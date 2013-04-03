07ac383fbc537498931a7f22e700366dc216bb19
Fareed Warrad <fwarrad@unca.edu>
2013-04-02 20:38:35 -0400

    first commit since completing express blog. Now will convert to project 4.

--- twitter.js
+++ twitter.js
@@ -1,5 +1,10 @@
+var http = require("http");
 var twitter = require('ntwitter');
+var redis = require('redis');
 var credentials = require('./credentials.js');
+
+//create redis client                                                                                                                                                                                                                       
+var client = redis.createClient();
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
@@ -14,6 +19,29 @@ t.stream(
     function(stream) {
         stream.on('data', function(tweet) {
             console.log(tweet.text);
+            //if awesome is in the tweet text, increment the counter                                                                                                                                                                        
+            if(tweet.text.match(/awesome/)) {
+                client.incr('awesome');
+            }
+            
+            if(tweet.text.indexOf("cool") > -1) {
+              client.incr("cool");
+            }
         });
     }
 );
+
+var server = http.createServer(function (req, res) {
+    client.mget(["awesome","cool"], function(err, results) {
+        if (err !== null) {
+            //handle error here
+            console.log("error: " + err);
+        } else {
+  	    var response = "<b>Hello from my http server!!</b>";
+	    response += "<p>Total awesome: " + results[0] + "</p>";
+	    response += "<p>Total cool: " + results[1] + "</p>";
+	    res.writeHead(200, {"Content-Type": "text/html"});
+	    res.end(response);
+        }
+    });
+}).listen(3000);
